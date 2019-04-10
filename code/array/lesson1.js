2ff2c197bdd8053f10a3e4cb3f7921c6b0be3760
jiuwo <694518723@qq.com>
2019-04-10 19:58:53 +0800

    add string1 array1

---
+++ code/array/lesson1.js
@@ -0,0 +1,34 @@
+export default (str) => {
+  // 建立电话号码键盘映射
+  let map = ['',1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+  // 把输入字符串按单字符分隔变成数组  234 => [2,3,4]
+  let num = str.split('')
+  // 保存键盘映射后的字母内容 如： 23=>['abc','def']
+  let code = []
+  num.forEach(item => {
+    if(map[item]) {
+      code.push(map[item])
+    }
+  })
+  let comb = (arr) => {
+    // 临时变量用来表示前两个组合的结果
+    let tmp = []
+    // 最外层的循环是遍历第一个元素， 里层的元素是遍历第二个元素
+    for (let i=0, il=arr[0].length; i<il; i++) {
+      for (let j=0, jl=arr[1].length; j<jl; j++) {
+        // 保存两个 组成 一个新 
+        tmp.push(`${arr[0][i]}${arr[1][j]}`)
+      }
+    }
+    // 从 0 开始 ['',1,] 替换掉， 用谁呢？ 新组合的临时变量里的值
+    arr.splice(0, 2, tmp)
+    if (arr.length>1) {
+      comb(arr)
+      } else {
+        return tmp
+      }
+    // 这个数组 最终只有一个元素
+    return arr[0]
+  }
+  return comb(code)
+}
